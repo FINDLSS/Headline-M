f2ea4c55ba3130f33fba77505a5df13452517ad8
W-kin <kaminosakura@163.com>
2022-05-27 10:41:25 +0800

    项目初始化 引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+
+    <i class="toutiao toutiao-shouji"></i>
+    <hr>
+    <van-button disabled type="primary">禁用状态</van-button>
+    <van-button disabled type="info">禁用状态</van-button>
     <!-- 下面是路由的占位符： -->
     <router-view />
   </div>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入vant核心组件库：
+import Vant from 'vant'
+import 'vant/lib/index.css'
 //加载全局样式表：
-import '@/style/index.less'
+import './style/index.less'
+
+//注册组件库：
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
